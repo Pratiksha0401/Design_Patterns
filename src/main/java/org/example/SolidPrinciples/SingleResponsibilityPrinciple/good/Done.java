6825e6601cd4c5d31e1e9cbb93eaf3c47f3cee09
Pratiksha0401 <pratiksha411998@gmail.com>
2025-04-05 15:49:36 +0530

    added main method

--- src/main/java/org/example/SolidPrinciples/SingleResponsibilityPrinciple/good/Done.java
+++ src/main/java/org/example/SolidPrinciples/SingleResponsibilityPrinciple/good/Done.java
@@ -1,5 +1,7 @@
 package org.example.SolidPrinciples.SingleResponsibilityPrinciple.good;
 
 class Done {
-
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
