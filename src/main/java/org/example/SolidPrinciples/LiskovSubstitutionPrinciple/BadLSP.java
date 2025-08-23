f1e70c7ac2375dc486d453580934d530a81ec413
Pratiksha Nagoshe <69256553+Pratiksha0401@users.noreply.github.com>
2025-08-23 19:18:58 +0530

    Update BadLSP.java

--- src/main/java/org/example/SolidPrinciples/LiskovSubstitutionPrinciple/BadLSP.java
+++ src/main/java/org/example/SolidPrinciples/LiskovSubstitutionPrinciple/BadLSP.java
@@ -1,4 +1,33 @@
 package org.example.SolidPrinciples.LiskovSubstitutionPrinciple;
 
 public class BadLSP {
+  public static void main(String[] args) {
+        Bird bird = new Sparrow();
+        bird.fly(); // Output: Sparrow is flying
+
+        bird = new Penguin();
+        bird.fly(); // Throws UnsupportedOperationException
+    }
 }
+
+
+class Bird {
+    public void fly() {
+        System.out.println("Bird is flying");
+    }
+}
+
+class Sparrow extends Bird {
+    @Override
+    public void fly() {
+        System.out.println("Sparrow is flying");
+    }
+}
+
+class Penguin extends Bird {
+    @Override
+    public void fly() {
+        throw new UnsupportedOperationException("Penguins can't fly");
+    }
+}
+
